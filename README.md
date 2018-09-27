50fd9ec43134c9d6afb4fab3f6733edfda838c39
Thomas Schersach <tschersach@gmail.com>
2018-09-27 10:19:04 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Pulumi Epsagon integration
+
+This package provides Epsagon integration with Pulumi programs. When imported into a Pulumi program, any serverless
+functions generated from JavaScript callbacks in the Pulumi program will automatically be wrapped with Epsagon Lambda Wrapper.
+
+```javascript
+const aws = require("@pulumi/aws");
+const serverless = require("@pulumi/aws-serverless");
+
+// Load the Pulumi IO| integration package
+require("@pulumi/iopipe");
+
+// Create a bucket and a function to log new object uploads
+const bucket = new aws.s3.Bucket("my-bucket");
+serverless.bucket.onPut("onNewObject", bucket, async (ev) => console.log(ev));
+exports.bucketName = bucket.bucket;
+```
+
+## Configuration
+
+After importing `@pulumi/epsagon` into a Pulumi program, you will need to provide an Epsagon token via a Pulumi configuration secret.  You can get your token on the "Install" page of the Epsagon console for your project.
+
+```bash
+$ pulumi config set --secret epsagon:token <your token here>
+```
---
+++ index.ts
@@ -0,0 +1,58 @@
+// Copyright 2016-2018, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as pulumi from '@pulumi/pulumi'
+
+// Require an `iopipe:token` to be provided by the end-user.
+const config = new pulumi.Config('epsagon')
+const token = config.require('token')
+
+export interface EpsagonConfig {
+    appName?: string;
+    metadataOnly?: boolean;
+    useSSL?: boolean;
+    traceCollectorURL?: string,
+    isEpsagonDisabled?: boolean,
+}
+
+// Replace `pulumi.runtime.serializeFunction` and `pulumi.runtime.computeCodePaths` with wrappers which injects Epsagon
+// around all serialized Pulumi functions.
+export function install(pul: typeof pulumi, epsagonConfig: EpsagonConfig = {}) {
+    const origSerializeFunction = pul.runtime.serializeFunction
+    pul.runtime.serializeFunction = async function (func, args) {
+        const wrapper = () => {
+            require('@epsagon/epsagon').init({
+                token,
+                appName: 'pulumi',
+                metadataOnly: false,
+                ...epsagonConfig
+            })
+            return require('@epsagon/epsagon').lambdaWrapper(func)
+        }
+        const serialized = await origSerializeFunction(wrapper, {...args, isFactoryFunction: true})
+        return {
+            ...serialized
+        }
+    }
+    const originComputeCodePaths = pul.runtime.computeCodePaths
+    pul.runtime.computeCodePaths = async function (extraIncludePaths, extraIncludePackages = [], extraExcludePackages) {
+        // Make sure that `@epsagon/epsagon` is included in the uploaded package.
+        const newExtraIncludePackages = [...extraIncludePackages, '@epsagon/epsagon']
+        return originComputeCodePaths(extraIncludePaths, newExtraIncludePackages, extraExcludePackages)
+    }
+}
+
+// Attempt to install to the version of Pulumi we loaded
+install(pulumi)
+
